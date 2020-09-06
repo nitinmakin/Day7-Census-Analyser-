af688169ace9e89df408eee5f987a71ad5bd38c0
nitinmakin <nitinmakin124.com>
2020-09-07 03:38:42 +0530

    Added-Load indian state code information from csv file

---
+++ src/main/java/com/bridgelabz/censusanalyser/model/CSVStatesCode.java
@@ -0,0 +1,26 @@
+package com.bridgelabz.censusanalyser.model;
+import com.opencsv.bean.CsvBindByName;
+
+public class CSVStatesCode {
+    @CsvBindByName(column = "State Name", required = true)
+    public String stateName;
+
+    @CsvBindByName(column = "StateCode", required = true)
+    public String stateCode;
+
+    @CsvBindByName(column = "SrNo", required = true)
+    public String SrNo;
+
+    @CsvBindByName(column = "TIN", required = true)
+    public String TIN;
+    @Override
+    public String toString() {
+        return "CSVStatesCode{" +
+                "stateName='" + stateName + '\'' +
+                ", stateCode='" + stateCode + '\'' +
+                ", SrNo='" + SrNo + '\'' +
+                ", TIN='" + TIN + '\'' +
+                '}';
+    }
+
+}
