5d038f32a0188bc7ffa5533e75bb708e54d6cf3b
nitinmakin <nitinmakin124.com>
2020-09-07 18:12:01 +0530

    Added-Load india state code info and commit all changes

--- src/main/java/com/bridgelabz/censusanalyser/service/StateCensusAnalyser.java
+++ src/main/java/com/bridgelabz/censusanalyser/service/StateCensusAnalyser.java
@@ -2,8 +2,10 @@ package com.bridgelabz.censusanalyser.service;
 
 import com.bridgelabz.censusanalyser.exception.CensusAnalyserException;
 import com.bridgelabz.censusanalyser.model.CSVStateCensus;
+import com.bridgelabz.censusanalyser.model.CSVStatesCode;
 import com.opencsv.bean.CsvToBean;
 import com.opencsv.bean.CsvToBeanBuilder;
+
 import java.io.Reader;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -18,12 +20,32 @@ public class StateCensusAnalyser {
             CsvToBean<CSVStateCensus> csvToBean = csvToBeanBuilder.build();
             Iterator<CSVStateCensus> censusCSVIterator = csvToBean.iterator();
 
-            int numOfEntries = 0;
+            int numberOfEntries = 0;
             while (censusCSVIterator.hasNext()) {
-                numOfEntries++;
+                numberOfEntries++;
                 censusCSVIterator.next();
             }
-            return numOfEntries;
+            return numberOfEntries;
+        } catch (Exception e) {
+            throw new CensusAnalyserException(e.getMessage(),
+                    CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM);
+        }
+    }
+
+    public int loadIndiaStateCodeCsv(String csvFilePath) throws CensusAnalyserException {
+        try (Reader reader = Files.newBufferedReader(Paths.get(csvFilePath))) {
+            CsvToBeanBuilder<CSVStatesCode> csvToBeanBuilder = new CsvToBeanBuilder<>(reader);
+            csvToBeanBuilder.withType(CSVStatesCode.class);
+            csvToBeanBuilder.withIgnoreLeadingWhiteSpace(true);
+            CsvToBean<CSVStatesCode> csvToBean = csvToBeanBuilder.build();
+            Iterator<CSVStatesCode> censusCSVIterator = csvToBean.iterator();
+
+            int numberOfEntries = 0;
+            while (censusCSVIterator.hasNext()) {
+                numberOfEntries++;
+                censusCSVIterator.next();
+            }
+            return numberOfEntries;
         } catch (Exception e) {
             throw new CensusAnalyserException(e.getMessage(),
                     CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM);
