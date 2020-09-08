21667c6ef64b0a0a1219a057b416f243e1da40a5
nitinmakin <nitinmakin124.com>
2020-09-08 21:31:25 +0530

    Refactored-Code to support list in place of iterator

--- src/main/java/com/bridgelabz/censusanalyser/service/StateCensusAnalyser.java
+++ src/main/java/com/bridgelabz/censusanalyser/service/StateCensusAnalyser.java
@@ -1,4 +1,5 @@
 package com.bridgelabz.censusanalyser.service;
+
 import com.bridgelabz.censusanalyser.exception.CensusAnalyserException;
 import com.bridgelabz.censusanalyser.model.CSVStateCensus;
 import com.bridgelabz.censusanalyser.model.CSVStatesCode;
@@ -6,27 +7,26 @@ import com.bridgelabz.csvbuilder.exception.CsvBuilderException;
 import com.bridgelabz.csvbuilder.service.CsvBuilderFactory;
 import com.bridgelabz.csvbuilder.service.ICsvBuilder;
 
+
 import java.io.IOException;
 import java.io.Reader;
-
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Iterator;
+import java.util.List;
 import java.util.stream.StreamSupport;
 
 public class StateCensusAnalyser {
     public int loadCsvData(String csvFilePath) throws CensusAnalyserException {
         try (Reader reader = Files.newBufferedReader(Paths.get(csvFilePath))) {
             ICsvBuilder csvBuilder = CsvBuilderFactory.createCsvBuilder();
-            Iterator<CSVStateCensus> censusCSVIterator = csvBuilder.getCSVFileIterator(reader, CSVStateCensus.class);
-            return this.getCount(censusCSVIterator);
+            List<CSVStateCensus> censusCSVList = csvBuilder.getCSVFileList(reader, CSVStateCensus.class);
+            return censusCSVList.size();
         } catch (Exception e) {
             throw new CensusAnalyserException(e.getMessage(),
                     CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM);
         }
     }
-
-
     public int loadIndiaStateCodeCsv(String csvFilePath) throws CensusAnalyserException {
         try (Reader reader = Files.newBufferedReader(Paths.get(csvFilePath))) {
             ICsvBuilder csvBuilder = CsvBuilderFactory.createCsvBuilder();
